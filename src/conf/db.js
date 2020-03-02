c1963535fac18f7c9ecee76eec226aa496402fc9
newlfpe <1009107904@qq.coml>
2020-03-02 14:46:25 +0800

    首次提交

---
+++ src/conf/db.js
@@ -0,0 +1,38 @@
+/**
+ * @description 存储配置
+ */
+const { isProd } = require('../utils/env')
+
+let REDIS_CONF = {
+    prot: 6379,
+    host: '127.0.0.1'
+}
+
+let MYSQL_CONF = {
+    host: '101.37.20.247',
+    user: 'root',
+    password: '123456',
+    port: '3306',
+    database: 'koa2_weibo_db'
+}
+
+//线上配置
+if (isProd) { 
+    REDIS_CONF = {
+        prot: 6379,
+        host: '127.0.0.1'
+    }
+
+    MYSQL_CONF = {
+        host: '101.37.20.247',
+        user: 'root',
+        password: '123456',
+        port: '3306',
+        database: 'koa2_weibo_db'
+    }
+}
+
+module.exports = {
+    REDIS_CONF,
+    MYSQL_CONF
+}
---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+	ctx.body = 'hello'
+})
+
+router.get('/string', async (ctx, next) => {
+	ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+	ctx.body = {
+		title: 'koa2 json'
+	}
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+	const { userName } = ctx.params
+	ctx.body = {
+		title: 'tj',
+		userName
+	}
+})
+
+module.exports = router
